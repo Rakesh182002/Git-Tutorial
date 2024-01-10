c24f057bcfda500b26de196a0829b965ab62a112
rakesh182002 <rakeshramesh182002@gmail.com>
2024-01-10 12:16:23 +0530

    change in branch1

--- src/com/jsp/gitbash/Demo.java
+++ src/com/jsp/gitbash/Demo.java
@@ -1,21 +1,5 @@
 package com.jsp.gitbash;
 
-public class Demo {
-	public static void main(String[] args) {
-		System.out.println("Normal hello");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-//	change 1
-//	change 2
+public class Demo {	
 
 }
