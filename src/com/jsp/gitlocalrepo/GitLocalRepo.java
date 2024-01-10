cd0df8d87bab88bc17098390a853a4cc7c0f82fa
Rahul Jaganathan <rahul637j@gmail.com>
2024-01-10 12:30:01 +0530

    Saved to Branch1

--- src/com/jsp/gitlocalrepo/GitLocalRepo.java
+++ src/com/jsp/gitlocalrepo/GitLocalRepo.java
@@ -1,4 +1,11 @@
 package com.jsp.gitlocalrepo;
 
-public class GitLocalRepo {
+public class GitLocalRepo 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world");
+		
+		
+	}
 }
